048c9f436384ae7a020d5b2a0ae04dc4fde2605c
illum1nadi <adityainduri@gmail.com>
2025-07-04 02:56:15 +0530

    configured routes of proj.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,17 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
+import { Poppins } from 'next/font/google'
+
 import "./globals.css";
 
 const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  variable: "--font-poppins",
-});
+    subsets: ['latin'],
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+    variable: '--font-poppins',
+})
 
 export const metadata: Metadata = {
   title: "StoreIt",
-  description: "The best place to store your files",
+  description: "StoreIt - The only storage solution you need.",
 };
 
 export default function RootLayout({
